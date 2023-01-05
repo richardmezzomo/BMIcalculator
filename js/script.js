48267a8bfc4850c5572e616db4de29887aaa4a64
richardbmezzomo <richardmezzomo1@gmail.com>
2023-01-05 17:33:44 -0300

    add a category for each BMI

--- js/script.js
+++ js/script.js
@@ -29,7 +29,18 @@ function handleSubmit (event) {
 form.oninput = () => AlertError.close()
 
 function displayResultMessage (result) {
-  const message = `Your BMI is ${result}`
+  let message;
+  if (result < 18.5) {
+    message = `Your BMI is ${result} and the category is: Underweight`
+  } else if (result > 18.5 && result < 24.9) {
+    message = `Your BMI is ${result} and the category is: Normal`
+  } else if (result > 25.0 && result < 29.9) {
+    message = `Your BMI is ${result} and the category is: Overweight`
+  } else if (result > 30.0 && result < 39.9) {
+    message = `Your BMI is ${result} and the category is: Obesity`
+  } else if (result > 40) {
+    message = `Your BMI is ${result} and the category is: Severe Obesity`
+  }
   
   Modal.message.innerText = message
   Modal.open()
